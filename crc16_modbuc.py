ce15c5bf75f906f44c544072ebde00a066991e52
sanjas12 <45870132+sanjas12@users.noreply.github.com>
2021-12-06 12:56:12 +0300

    Add files via upload
    Add crc-modbus.py

---
+++ crc16_modbuc.py
@@ -0,0 +1,20 @@
+def crc16(data: str, poly: hex = 0xA001) -> str:
+    '''
+        CRC-16 MODBUS HASHING ALGORITHM
+    '''
+    crc = 0xFFFF
+    for byte in data:
+        # print(byte, ord(byte), hex(ord(byte)))
+        crc ^= ord(byte)
+        # print(crc)
+        for _ in range(8):
+            crc = ((crc >> 1) ^ poly
+                   if (crc & 0x0001)
+                   else crc >> 1)
+
+    hv = hex(crc).upper()[2:]
+    blueprint = '0000'
+    return (blueprint if len(hv) == 0 else blueprint[:-len(hv)] + hv)
+
+
+print(crc16('123456789'))
