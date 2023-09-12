eae9667d8a5d3cd90d6dad12dab71ce6d9972ecd
diakont_notebook <sanja100v@gmail.com>
2023-09-12 15:38:22 +0300

    Add ADAM 6017.py

---
+++ ADAM_6017.py
@@ -0,0 +1,23 @@
+from pyModbusTCP.client import ModbusClient
+import time
+
+SERVER_HOST = "192.168.30.11"
+
+cl = ModbusClient(host=SERVER_HOST)
+
+
+def dec_to_ma(dec):
+    ValMax, ValMin = 20.0, 4.0
+    Amax, Amin = 65535,  0.0
+    shift = 0.0 
+    ma = ((ValMax-ValMin)*(dec-Amin))/(Amax-Amin) + (ValMin + shift) 
+    return ma
+
+while True:
+    data_read = cl.read_holding_registers(1, 8)     # 1 - соответствует адресу 40001  по ADAM-6000_User_Manaul_Ed.10-FINAL.pdf
+                                                    #  стр. 217
+    print(f'data from ADAM_6017 ch_0:  {data_read[0]}, {round(dec_to_ma(data_read[0]), 2)} mA')
+    time.sleep(0.1)
+
+
+
