546d696a4a614ab2a2f597e2722a1616cc4010d8
diakont_notebook <sanja100v@gmail.com>
2023-10-24 16:23:10 +0300

    Update pi_32d

--- modbus_rtu/pi_32d.py
+++ modbus_rtu/pi_32d.py
@@ -5,38 +5,35 @@ import serial
 import struct
 import time
 
+def open_com(port='COM2'):
+    try:
+        ser = serial.Serial(port=port, baudrate=19200, timeout=1, bytesize=serial.EIGHTBITS, stopbits=1)
+    except serial.SerialException:
+        print(f"could not open {port}")
+        time.sleep(1) 
+    else:
+        return ser    # если удалось открыть com порт
 
 def request()  -> None:
-    ser = serial.Serial('COM2', 19200, timeout=1, bytesize=serial.EIGHTBITS, stopbits=1)
-
-    # стандартный запрос регистров ввода в 16-ричном формате modbus rtu (01 04 0000 0012 7007)
-    # 01 = адрес, 
-    # 04 - регистры ввода
-    # 0000 - стартовый адрес, 
-    # 0012 - 18 значений, контрольная сумма crc16 modbus переворачивается
-    # 7007 crc - https://crccalc.com/ - расчет crc)
-    if ser.isOpen(): 
+    ser = open_com()
+    if ser:
+        # стандартный запрос регистров ввода в 16-ричном формате modbus rtu (01 04 0000 0012 7007)
+        # 01 = адрес, 
+        # 04 - регистры ввода
+        # 0000 - стартовый адрес, 
+        # 0012 - 18 значений, контрольная сумма crc16 modbus переворачивается
+        # 7007 crc - https://crccalc.com/ - расчет crc)
         request = bytes.fromhex('02 04 0000 0001 31F9')
         ser.write(request)
 
-    data = ser.readall()  # ответ в байтах
-
-def all_response(data: bytes):
-
-    temperature = data[12]/10
-    print('Teмпература:', temperature)
-
-    # print(data[3:7].hex('.'))     # convert byte to hex
-
-    high_word = bytearray(data[3:5])
-    high_word.reverse()
-    low_word = bytearray(data[5:7])
-    low_word.reverse()
-    torque = struct.unpack('f', high_word+low_word)[0]
-    print('Крутящий момент:', torque)
+def response(ser):
+    if ser:
+        data = ser.readall()
+        print('data', data)
 
 
 if __name__ == '__main__':
     while True:
-        response = request()        # делим на 10 в описании так сказано
-        print(time.ctime(), repr(response))        # делим на 10 в описании так сказано
+        request()
+        response(open_com())
+        # print(time.ctime())        # делим на 10 в описании так сказано
